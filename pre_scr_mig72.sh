4e6a0b16921010c9c968b25b4fb3cfa62587035d
simplyunix <eyetnz@gmail.com>
2021-06-21 11:37:43 +1200

    Added mksysb, pre migration checks and alt_copy commands

--- pre_scr_mig72.sh
+++ pre_scr_mig72.sh
@@ -49,6 +49,26 @@ echo "####### Run the pre migration checks and review the outputs"
 
 echo "####### Backup up the system via mksysb to a remote filesystems"
 mksysb -i $savedir/`hostname`.pre_migration.mksysb
+
+echo "####### Backup the NIM database"
+/usr/lpp/bos.sysmgt/nim/methods/m_backup_db '/stage/kapnim/nimmast.nim.db.backup'
+
+echo "####### Lets create a clone of rootvg with a -B flag"
+echo "–B flag. So that the bootlist is not changed, as I don’t \
+want to boot on the cloned disk (it will only be used if the migration were to fail)."
+lspv | grep old_rootvg
+echo
+echo "Lets clean the ODM.... Remove to free up this disk"
+echo
+lspv | grep old_rootvg| awk '{print $1}'| while read hdisk
+do
+  echo "Removing $hdisk"
+  echo alt_rootvg_op -X old_rootvg
+  echo alt_disk_copy -Bd $hdisk
+  echo
+  lspv
+done
+
 }
 
 main 2>&1 | tee $logname
